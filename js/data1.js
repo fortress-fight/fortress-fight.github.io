cd0132ef6aa26cccd60aee80fa67c02c2806202e
fortress-fighting <dreamersfly@163.com>
2017-01-16 02:22:22 +0800

    调整数据

--- js/data1.js
+++ js/data1.js
@@ -1,19 +1,20 @@
 var	pageData = [
 	{
 		index: 0,
-		title: 'Uncommon Talent',
+		title: 'HTML/CSS',
 		con: [
 			{
-				h: 'At Adison Partners, we know what uncommon talent looks like.',
+				h: 'You cannot leave ,real warrior never quits.',
 				p: [
-					'As technological, social and cultural trends impact the global enterprise, organizations are seeking a new kind of talent that is a better fit for the future. “Uncommon talent” is a term used to describe high-value individuals with unique, innovative skills that transcend industries.',
-					'Uncommon talent includes innovative and original thinkers with an intellectual curiosity that constantly challenges the status quo.'
+					'--Kung Fu Panda',
+					' (0-0) ',
+					'HTML 和 CSS'
 				]
 			},
 			{
 				p: [
-					'These driven individuals demonstrate the propensity to continuously learn new things and apply these learnings to deftly adapt to business critical opportunities.',
-					'While other industry recruiters are focused on re-using the same, limited pool of candidates, we focus on the identification and conversion of fresh, top talent.',
+					'学习是件辛苦的事情，但是收获会使人快乐。',
+					'',
 					'Our candidates embody a broad mix of characteristics including creativity and operational know-how, paired with deep, industry-agnostic business experience.'
 				]
 			},
@@ -27,7 +28,7 @@ var	pageData = [
 	},
 	{
 		index: 1,
-		title: 'Executive Search',
+		title: 'HTML5',
 		con: [
 			{
 				h: 'Approach',
@@ -58,7 +59,7 @@ var	pageData = [
 	},
 	{
 		index: 2,
-		title: 'Uncommon Talent',
+		title: 'CSS3',
 		con: [
 			{
 				h: 'At Adison Pent looks like.',
@@ -86,7 +87,7 @@ var	pageData = [
 	},
 	{
 		index: 3,
-		title: 'Uncommon Talent',
+		title: 'PROJECT',
 		con: [
 			{
 				h: 'At Adison Partners, we know what uncommon talent looks like.',
@@ -113,7 +114,7 @@ var	pageData = [
 var	pageData1 = [
 	{
 		index: 0,
-		title: 'On Demand Consulting',
+		title: 'JavaScript',
 		con: [
 			{
 				h: 'At Adison Partners, we know what uncommon talent looks like.',
@@ -139,7 +140,7 @@ var	pageData1 = [
 	},
 	{
 		index: 1,
-		title: 'Areas of Focus',
+		title: 'Reading Book',
 		con: [
 			{
 				h: 'Approach',
@@ -201,12 +202,12 @@ var	pageData1 = [
 var	pageData2 = [
 	{
 		index: 0,
-		title: 'Fortune 500 Food',
+		title: 'About Me',
 		con: [
 			{
 				h: 'At Adison Partners, we know what uncommon talent looks like.',
 				p: [
-					'As technological, social and cultural trends impact the global enterprise, organizations are seeking a new kind of talent that is a better fit for the future. “Uncommon talent” is a term used to describe high-value individuals with unique, innovative skills that transcend industries.',
+					'我知道自己还不够好，但是我也知道我会变得更好',
 					'Uncommon talent includes innovative and original thinkers with an intellectual curiosity that constantly challenges the status quo.'
 				]
 			},
@@ -227,23 +228,24 @@ var	pageData2 = [
 	},
 	{
 		index: 1,
-		title: 'Leading Luxury Automotive ',
+		title: 'RESUME',
 		con: [
 			{
 				h: 'Approach',
 				p: [
-					'Our proprietary sourcing and assessment process is uniquely designed to seek, attract, and qualify extraordinary talent. Our methods have been created and refined through analyzing the performance and trajectory of thousands of candidates worldwide.',
+					'第一句 HelloWorld，到第一页面、第一个特效、第一个 repository ...... 到如今我也踏上了白雪茫茫的求值之路。 一路走来让人不禁感慨时光荏苒；',
 					'Our initial sourcing strategy is driven by a client’s current and desired corporate culture, differentiating competencies, functional requirements, and critical experiences. These insights drive us to find the very best uncommon candidates.',
 					'Our assessment criteria are customized to blend unique client qualifications along with our knowledge of market excellence and ‘differentiators of uncommon talent.’ Assessment results produce candidate comparisons and insights that enable well-informed hiring decisions based on specific client needs and desire to hire the extraordinary.'
 				]
 			},
 			{
-				h: 'Speed',
+				h: '前端是个深坑',
 				p: [
-					'The pace of play is clearly changing. From new product launches to supply chain and inventory cycles and customer insights, our clients are on a constant quest for speed, quality and innovation. Our belief is that you should expect the same from your recruiting cycle.',
-					'We believe the industry benchmark of 90-days to complete a single search reflects stale thinking and the inability to use technology, strong partner relationships and client focus to redefine what’s possible. At Adison, we hold ourselves to a higher standard that is designed to produce the quality you deserve in the timeframe you need.',
-					'Within 14 days of engaging Adison, we commit to our clients a slate of 2-3 fully vetted “bulls-eye” candidates. Globally, we have accomplished this for billion dollar enterprises and start-ups looking for diverse, extraordinary talent.'
+					'前端是个深坑，即便是有茫茫多的大神指路，也难免在无数个陷阱中摔了个七荤八素的，只能在事后感叹一句：又被教做人了；',
+					'在爬坑的过程中，我渐渐地明白了：对知识一定要有敬畏之心',
+					'我能做的还有很多，我能学的还有很多，如果学的多那么我能做的就更多了'
 				]
+
 			},
 			{
 				h: 'Commitment',
@@ -258,28 +260,30 @@ var	pageData2 = [
 	},
 	{
 		index: 2,
-		title: 'Global Industrial Brand',
+		title: 'Future',
 		con: [
 			{
-				h: 'At Adison Pent looks like.',
+				h: 'About  Future',
 				p: [
-					'As technological, social and cultural trends impact the global enterprise, organizations are seeking a new kind of talent that is a better fit for the future. “Uncommon talent” is a term used to describe high-value individuals with unique, innovative skills that transcend industries.',
-					'Uncommon talent includes innovative and original thinkers with an intellectual curiosity that constantly challenges the status quo.'
+					'走到今天，我的心态也几经起伏；如今平静下来后，才发现自己刚刚看到那没有尽头的学习之路；',
+					'向前看有HTML5、ES6、React、ReactNative、Vue、NodeJs……向后看有设计模式、算法、HTTP、计算机基础……低低头还能看到Canvas、JavaScript、jQuery、Sass……',
+					'(说着说着都想哭了)'
 				]
 			},
 			{
-				h: 'At Adison Part talent looks like.',
+				h: '送给自己',
 				p: [
-					'These driven individuals demonstrate the propensity to continuously learn new things and apply these learnings to deftly adapt to business critical opportunities.',
-					'While other industry recruiters are focused on re-using the same, limited pool of candidates, we focus on the identification and conversion of fresh, top talent.',
-					'Our candidates embody a broad mix of characteristics including creativity and operational know-how, paired with deep, industry-agnostic business experience.'
+					'如果你还有时间，如果你还能奋斗，那么这就是你最好的时代'
 				]
 			},
 			{
-				h: 'At Adison Pwhat uncommon talent looks like.',
+				h: '未来',
 				p: [
-					'Our approach provides a new lens for identifying, converting, and placing talent to spark innovation, inspiration, and creativity in organizations on the brink of change.',
-					'Our mission is to identify, source and cultivate the uncommon talent that empowers your organization to expand its reach, broaden capabilities and reinvent business for a new era.'
+					'时光流逝，已经醒来的人总不能再继续昏昏欲睡，希望能在下一个阶段能够完成以下目标：',
+					'1. 找到一个工作，并能够保持饥饿的状态，继续学习',
+					'2. 继续学习JavaScript',
+					'3. 能够熟练使用React',
+					'4. 入门NodeJs'
 				]
 			}
 		]
