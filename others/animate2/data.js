4e729152c1dfd72e0050212d4bddfe38dc9bb36d
fortress-fighting <dreamersfly@163.com>
2017-03-08 01:48:10 +0800

    1.0.0

---
+++ others/animate2/data.js
@@ -0,0 +1,215 @@
+var data = {
+    imgData: [{
+            path: './img/1.png',
+            width: 199,
+            height: 624,
+            disX: 200,
+            disY: 0,
+            disZ: 100,
+            scale: 1
+        },
+        {
+            path: './img/2.png',
+            width: 207,
+            height: 612,
+            disX: 400,
+            disY: 0,
+            disZ: 103,
+            scale: 1,
+        },
+        {
+            path: './img/3.png',
+            width: 389,
+            height: 651,
+            disX: 400,
+            disY: 100,
+            disZ: 106,
+            scale: 1,
+        },
+        {
+            path: './img/4.png',
+            width: 492,
+            height: 592,
+            disX: 300,
+            disY: 300,
+            disZ: 110,
+            scale: 1,
+        },
+        {
+            path: './img/5.png',
+            width: 493,
+            height: 611,
+            disX: 200,
+            disY: -600,
+            disZ: 115,
+            scale: 1,
+        },
+        {
+            path: './img/6.png',
+            width: 130,
+            height: 129,
+            disX: 300,
+            disY: -100,
+            disZ: 120,
+            scale: 1,
+        },
+        {
+            path: './img/7.png',
+            width: 213,
+            height: 228,
+            disX: 300,
+            disY: 100,
+            disZ: 122,
+            scale: 1,
+        },
+        {
+            path: './img/11.png',
+            width: 382,
+            height: 434,
+            disX: 200,
+            disY: 400,
+            disZ: 128,
+            scale: 1,
+        },
+        {
+            path: './img/12.png',
+            width: 403,
+            height: 313,
+            disX: 300,
+            disY: -100,
+            disZ: 132,
+            scale: 1,
+        },
+    ]
+}
+
+/**
+ * ,
+        {
+            path: './img/4.png',
+            width: 492,
+            height: 592,
+            disX: 0,
+            disY: 0
+        },
+        
+        {
+            path: './img/5.png',
+            width: 493,
+            height: 611,
+            disX: 0,
+            disY: 0
+        },
+        {
+            path: './img/6.png',
+            width: 130,
+            height: 129,
+            disX: 0,
+            disY: 0
+        },
+        {
+            path: './img/7.png',
+            width: 213,
+            height: 228,
+            disX: 0,
+            disY: 0
+        },
+        {
+            path: './img/8.png',
+            width: 903,
+            height: 643,
+            disX: 0,
+            disY: 0
+        },
+        {
+            path: './img/10.png',
+            width: 226,
+            height: 131,
+            disX: 0,
+            disY: 0
+        },
+        {
+            path: './img/9.png',
+            width: 654,
+            height: 470,
+            disX: 0,
+            disY: 0
+        },
+        {
+            path: './img/11.png',
+            width: 382,
+            height: 434,
+            disX: 0,
+            disY: 0
+        },
+        {
+            path: './img/12.png',
+            width: 403,
+            height: 313,
+            disX: 0,
+            disY: 0
+        },
+        {
+            path: './img/16.png',
+            width: 527,
+            height: 395,
+            disX: 0,
+            disY: 0
+        },
+        {
+            path: './img/14.png',
+            width: 429,
+            height: 600,
+            disX: 0,
+            disY: 0
+        },
+        {
+            path: './img/17.png',
+            width: 290,
+            height: 299,
+            disX: 0,
+            disY: 0
+        },
+        {
+            path: './img/18.png',
+            width: 464,
+            height: 539,
+            disX: 0,
+            disY: 0
+        },
+        {
+            path: './img/13.png',
+            width: 750,
+            height: 445,
+            disX: 0,
+            disY: 0
+        },
+        {
+            path: './img/19.png',
+            width: 440,
+            height: 471,
+            disX: 0,
+            disY: 0
+        },
+        {
+            path: './img/20.png',
+            width: 200,
+            height: 208,
+            disX: 0,
+            disY: 0
+        },
+        {
+            path: './img/22.png',
+            width: 298,
+            height: 296,
+            disX: 0,
+            disY: 0
+        },
+        {
+            path: './img/23.png',
+            width: 573,
+            height: 361,
+            disX: 0,
+            disY: 0
+        }
+ */
